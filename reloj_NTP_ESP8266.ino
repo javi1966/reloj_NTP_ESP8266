3c8f45dbab1045a23a0a4fb617692f523e5111ac
javi1966 <JEG1966@telefonica.net>
2020-04-10 10:32:16 +0200

    Puesta de termometro

--- reloj_NTP_ESP8266.ino
+++ reloj_NTP_ESP8266.ino
@@ -1,20 +1,32 @@
-
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //http://myshop.biz.ua/index.php?route=information/news/info&news_id=21
 
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
+#include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
+#include <NTPClient.h>
+#include <WiFiUDP.h>
+#include <ArduinoJson.h>
+#include <Ticker.h>
 #include "TM1637.h"
 //#include <TM1637Display.h>
-#include <Ticker.h>
+
 
 Ticker flipper;
 TM1637 tm1637(0, 2);              // CLK, DIO (D6, D5)
 //TM1637Display display(0, 2);
-#define GMT 2                       // INVIERNO GMT=1 ,VERANO GMT=2
+
+
+#define NTP_OFFSET   0 * 60 * 60 // In seconds
+#define NTP_INTERVAL 60 * 1000 // In miliseconds
+#define NTP_ADDRESS  "es.pool.ntp.org"
+
 #define brightness 4
 
+#define JSON_BUFF_DIMENSION 2500
+
+const int LED = 0;
 char ssid[] = "Wireless-N";             //  your network SSID (name)
 char pass[] = "z123456z";         // your network password
 //char ssid[] = "WLAN_BF";             //  your network SSID (name)
@@ -23,147 +35,83 @@ char pass[] = "z123456z";         // your network password
 
 byte hour, minute, second;
 boolean point;
+String hora;
+String minuto;
+const long timerUpdate = 60L;  //medio minuto
+const long timer5seg = 20 ;    //5 seg
+bool  flagUpdate = false;
+bool flagUpdate5seg = false;
+bool flag1hora=false;
+bool tarea = false;
+int valorH;
+int valorM;
+int temperatura;
+String text;
+const char server[] = "api.openweathermap.org";
+int jsonend = 0;
+boolean startJson = false;
 
-unsigned int localPort = 2390;      // local port to listen for UDP packets
-
-/* Don't hardwire the IP address or we won't get the benefits of the pool. *  Lookup the IP address for the host name instead */
-//IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
-IPAddress timeServerIP; // time.nist.gov NTP server address
-const char* ntpServerName = "time.nist.gov";
-//const char* ntpServerName="ntp.i2t.ehu.es";
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
 
 // A UDP instance to let us send and receive packets over UDP
-WiFiUDP udp;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
 
-void flip() {
+WiFiClient client;
+
+
+
+
+//************************************************************************************************
+void TimingISR()
+{
+  static long cntTemp = 0L;
+  static long cntT5seg = 0L;
+  static long cntT1hora = 0L;
+
+
+  //digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
   point = !point;
-  second++;
-  if (second > 59) {
-    second = 0;
-    minute++;
+  
+  if (++cntT1hora > 7200 )  //7200
+  {
+     flag1hora=true;
+
+     cntT1hora = 0;
   }
-  if (minute > 59) {
-    minute = 0;
-    hour++;
+  
+
+
+  if (++cntTemp > timerUpdate)
+  {
+    flagUpdate = true;
+
+    cntTemp = 0L;
   }
-  if (hour > 23) {
-    hour = 0;
+
+  if (++cntT5seg > timer5seg)
+  {
+    flagUpdate5seg = true;
+
+    cntT5seg = 0L;
   }
+
+  timer0_write(ESP.getCycleCount() + 40000000L); // 80MHz == 1sec
 
 }
-
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  Serial.println("sending NTP packet...");
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  udp.beginPacket(address, 123); //NTP requests are to port 123
-  udp.write(packetBuffer, NTP_PACKET_SIZE);
-  udp.endPacket();
-}
-
-
-void oldloop()
-{
-  //get a random server from the pool
-  WiFi.hostByName(ntpServerName, timeServerIP);
-
-  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-  // wait to see if a reply is available
-  delay(1500);
-
-  int cb = udp.parsePacket();
-  if (!cb) {
-    Serial.println("no packet yet");
-     __asm__("nop\n\t");  
-  }
-  else {
-    Serial.print("packet received, length=");
-    Serial.println(cb);
-    // We've received a packet, read the data from it
-    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = " );
-    Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
-
-   
-    epoch = epoch + GMT * 3600 ;
-
-    hour = (epoch  % 86400L) / 3600;
-    minute = (epoch  % 3600) / 60;
-    second = epoch % 60;
- 
-   
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-    Serial.print(':');
-    if ( ((epoch % 3600) / 60) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    Serial.print(':');
-    if ( (epoch % 60) < 10 ) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch % 60); // print the second
-  }
-  // wait ten seconds before asking for the time again
- 
-
-}
-
+//*********************************************************************************************************
 
 void setup()
 {
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
 
   tm1637.set(5);
   tm1637.init(D4036B);               ///tm1637
-  
+
   //display.setBrightness(0x0f);
- // display.showNumberDec(8888);  //debug
+  // display.showNumberDec(8888);  //debug
 
-  flipper.attach(1, flip);          /// Ð¿Ñ€ÐµÑ€Ñ‹Ð²Ð°Ð½Ð¸Ðµ 1 Ñ�ÐµÐº
-
-  Serial.println();
   Serial.println();
 
   // We start by connecting to a WiFi network
@@ -174,42 +122,139 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(1500);
-    //WiFi.begin(ssid, pass);
+
     Serial.print("*");
   }
+
   Serial.println("");
 
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  Serial.println("Starting UDP");
-  udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(udp.localPort());
+  timeClient.begin();
+  timeClient.setTimeOffset(7200);
+  //timeClient.setTimeOffset(3600);
+  dameHora();
 
-  oldloop();
+  reqOpenweather();
+
+  noInterrupts();
+  timer0_isr_init();
+  timer0_attachInterrupt(TimingISR);
+  timer0_write(ESP.getCycleCount() + 40000000L); // 80MHz == 1sec
+  interrupts();
 }
 
-void loop() {
-  if (second == 30) {
-    flipper.detach();
-    delay(1500);
-    oldloop();
-    flipper.attach(1, flip);
+//*******************************************************************************************************
+void reqOpenweather() {
+
+
+  HTTPClient http;  //Object of class HTTPClient
+  http.begin("http://api.openweathermap.org/data/2.5/forecast?q=sevilla,ES&APPID=c2ecf0a83c555c2054704fd94ff29f9e&mode=json&units=metric&cnt=2");
+  int httpCode = http.GET();
+  //Check the returning code
+  if (httpCode > 0) {
+
+    Serial.println(http.getString());
+    const size_t capacity = 2 * JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(2) + 4 * JSON_OBJECT_SIZE(1) + 3 * JSON_OBJECT_SIZE(2) + 2 * JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + 2 * JSON_OBJECT_SIZE(7) + JSON_OBJECT_SIZE(8) + 2 * JSON_OBJECT_SIZE(9) + 590;
+    DynamicJsonDocument doc(capacity);
+
+    DeserializationError error = deserializeJson(doc, http.getString());
+
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      return;
+    }
+
+    JsonObject list_0 = doc["list"][0];
+    JsonObject list_0_main = list_0["main"];
+    temperatura = list_0_main["temp"];
+
+    Serial.println(temperatura);
+
+
+    http.end();
   }
 
-  int8_t TimeDisp[4];
-  tm1637.point(point);
-  
-
-  TimeDisp[0] = hour / 10;
-  TimeDisp[1] = hour % 10;
-  TimeDisp[2] = minute  / 10;
-  TimeDisp[3] = minute  % 10;
-  //display.showNumberDecEx(hour,0x20,true, 2, 0);
-  //display.showNumberDec(minute, true, 2, 2);
- 
-  tm1637.display(TimeDisp);
+  else {
+    // if no connction was made:
+    Serial.println("connection failed");
+    return;
+  }
 }
 
+//*********************************************************************************************************
+void dameHora(){
+
+    timeClient.update();
+    String formattedTime = timeClient.getFormattedTime();
+    // Serial.println(formattedTime);
+
+    int split = formattedTime.indexOf(":");
+    hora = formattedTime.substring(0, split);
+    minuto = formattedTime.substring(split + 1, split + 3);
+   // Serial.println("Hora Actual: " + hora + ":" + minuto);
+
+    valorH = String(hora).toInt();
+    valorM =  String(minuto).toInt();
+
+}
+
+
+
+//*******************************************************************************************************
+
+void loop() {
+
+  int8_t TimeDisp[4];
+
+  if (flagUpdate ) {  //cada minuto
+
+    dameHora();
+
+
+    flagUpdate = false;
+
+  }
+
+  if(flag1hora){
+
+    reqOpenweather();
+    
+    flag1hora=false;
+  }
+
+  if (flagUpdate5seg) {
+
+  //  Serial.println("5 seg");
+    tarea = !tarea;
+    flagUpdate5seg = false;
+  }
+
+  switch (tarea) {
+
+    case 0:
+
+      tm1637.point(point);
+      TimeDisp[0] = valorH / 10;
+      TimeDisp[1] = valorH % 10;
+      TimeDisp[2] = valorM  / 10;
+      TimeDisp[3] = valorM  % 10;
+
+      tm1637.display(TimeDisp);
+      break;
+    case 1:
+      tm1637.point(0);
+      TimeDisp[0] = temperatura / 10;
+      TimeDisp[1] = temperatura % 10;
+      TimeDisp[2] = 16;
+      TimeDisp[3] = 12;  //es el indice en el array static int8_t TubeTab[] en TM1637.cpp
+      tm1637.display(TimeDisp);
+      break ;
+
+    default: break;
+  }
+
+}
